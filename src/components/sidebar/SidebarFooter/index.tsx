654c0b64b556685792660fbf287490add69134ea
ajimeno04 <jimenobeltran11@gmail.com>
2024-10-10 10:10:48 +0200

    feat: Add new features suggestions button and supported by protofire

--- src/components/sidebar/SidebarFooter/index.tsx
+++ src/components/sidebar/SidebarFooter/index.tsx
@@ -13,11 +13,16 @@ import {
 // import { openCookieBanner } from '@/store/popupSlice'
 // import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
 import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
-import { ListItem } from '@mui/material'
+import { Link, ListItem, SvgIcon, Typography } from '@mui/material'
 import DebugToggle from '../DebugToggle'
-import { HELP_CENTER_URL, IS_PRODUCTION } from '@/config/constants'
+import { HELP_CENTER_URL, IS_PRODUCTION, NEW_SUGGESTION_FORM } from '@/config/constants'
 import Track from '@/components/common/Track'
 import { OVERVIEW_EVENTS } from '@/services/analytics'
+import darkPalette from '@/components/theme/darkPalette'
+import { BEAMER_SELECTOR } from '@/services/beamer'
+import SuggestionIcon from '@/public/images/sidebar/lightbulb_icon.svg'
+import ProtofireLogo from '@/public/images/protofire-logo.svg'
+
 // import { useCurrentChain } from '@/hooks/useChains'
 
 const SidebarFooter = (): ReactElement => {
@@ -75,6 +80,35 @@ const SidebarFooter = (): ReactElement => {
           </a>
         </ListItem>
       </Track>
+      <Track {...OVERVIEW_EVENTS.SUGGESTIONS}>
+        <ListItem disablePadding>
+          <a target="_blank" rel="noopener noreferrer" href={NEW_SUGGESTION_FORM} style={{ width: '100%' }}>
+            <SidebarListItemButton id={BEAMER_SELECTOR} style={{ backgroundColor: '#12FF80', color: 'black' }}>
+              <SidebarListItemIcon color="primary">
+                <SuggestionIcon />
+              </SidebarListItemIcon>
+              <SidebarListItemText bold>New Features Suggestion?</SidebarListItemText>
+            </SidebarListItemButton>
+          </a>
+        </ListItem>
+      </Track>
+
+      <ListItem>
+        <SidebarListItemText>
+          <Typography variant="caption">
+            Supported by{' '}
+            <SvgIcon
+              component={ProtofireLogo}
+              inheritViewBox
+              fontSize="small"
+              sx={{ verticalAlign: 'middle', mx: 0.5 }}
+            />
+            <Link href="https://protofire.io" sx={{ color: darkPalette.primary.main, textDecoration: 'none' }}>
+              Protofire
+            </Link>
+          </Typography>
+        </SidebarListItemText>
+      </ListItem>
     </SidebarList>
   )
 }
