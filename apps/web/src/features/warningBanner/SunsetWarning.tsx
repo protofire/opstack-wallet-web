3e8f4d5d9743f1529b0715d59376ed113597a499
ajimeno04 <jimenobeltran11@gmail.com>
2025-09-23 13:51:29 +0200

    fix: update warning message in SunsetWarning component to reflect discontinuation of Soneium Minato testnet support

--- apps/web/src/features/warningBanner/SunsetWarning.tsx
+++ apps/web/src/features/warningBanner/SunsetWarning.tsx
@@ -10,7 +10,7 @@ export const SunsetWarning = () => {
   if (!isFeatureEnabled) return null
 
   return (
-    <ErrorMessage level="warning" title="Game7 is migrating its on-chain operations to Arbitrum One">
+    <ErrorMessage level="warning" title="Soneium Minato testnet support discontinuation ">
       <Typography display="inline" mr={1}>
         Dear users, Support for Soneium Minato will end soon. Network will be delisted from Superchain Safe UI by the
         end of September 2025. All deployed safes are left intact and will be available for interaction through Safe
