3f89a4b67146d4392af74544696a4ac6283b1a22
ajimeno04 <jimenobeltran11@gmail.com>
2025-09-24 12:06:20 +0200

    fix: update SunsetWarning component to include link to Safe CLI in discontinuation message for Soneium Minato testnet

--- apps/web/src/features/warningBanner/SunsetWarning.tsx
+++ apps/web/src/features/warningBanner/SunsetWarning.tsx
@@ -1,6 +1,6 @@
 import ErrorMessage from '@/components/tx/ErrorMessage'
 import { Typography } from '@mui/material'
-//import ExternalLink from '@/components/common/ExternalLink'
+import ExternalLink from '@/components/common/ExternalLink'
 import { useHasFeature } from '@/hooks/useChains'
 import { FEATURES } from '@safe-global/utils/utils/chains'
 
@@ -13,8 +13,8 @@ export const SunsetWarning = () => {
     <ErrorMessage level="warning" title="Soneium Minato testnet support discontinuation ">
       <Typography display="inline" mr={1}>
         Dear users, Support for Soneium Minato will end soon. Network will be delisted from Superchain Safe UI by the
-        end of September 2025. All deployed safes are left intact and will be available for interaction through Safe
-        CLI.
+        end of September 2025. All deployed safes are left intact and will be available for interaction through{' '}
+        <ExternalLink href="https://docs.safe.global/advanced/cli-overview">Safe CLI</ExternalLink>.
       </Typography>
     </ErrorMessage>
   )
