a6fb482e707baf8cf45ab5d561be515bd79aae93
Nikita Zasimuk <nikita.zasimuk@protofire.io>
2026-05-18 14:27:38 +0300

    feat: add a sunset banner for Swell networks

--- apps/web/src/components/dashboard/SunsetBanner/index.tsx
+++ apps/web/src/components/dashboard/SunsetBanner/index.tsx
@@ -3,9 +3,31 @@ import { Typography } from '@mui/material'
 import ExternalLink from '@/components/common/ExternalLink'
 import Link from 'next/link'
 import { AppRoutes } from '@/config/routes'
+import { useCurrentChain } from '@/hooks/useChains'
 
 export const SunsetBanner = () => {
-  // Warning should be visible across all networks
+  const currentChain = useCurrentChain()
+
+  // Swell Mainnet and Testnet will be sunset on June 30, 2026
+  if (currentChain?.chainId === '1923' || currentChain?.chainId === '1924') {
+    return (
+      <ErrorMessage level="warning" title="Swell Support Sunsetting">
+        <Typography display="inline" mr={1}>
+          Dear users, Support for Swell Networks will end soon. Please withdraw your funds until June 1, 2026. For
+          further details regarding the shutdown, please refer to this page (
+          <ExternalLink
+            href="https://x.com/swellnetworkio/status/2049065334536016382"
+            sx={{ '& > span': { textDecoration: 'underline' } }}
+          >
+            Link
+          </ExternalLink>
+          ).
+        </Typography>
+      </ErrorMessage>
+    )
+  }
+
+  // Warning should be visible across all other networks
   return (
     <ErrorMessage level="warning" title="Superchain Safe is merging with Protofire Safe!">
       <Typography display="inline" mr={1}>
